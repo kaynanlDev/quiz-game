4b47010a60e96d6de95f8ae1d1f5b8997ac16b8b
kaynanDev <kaynan2817@gmail.com>
2025-04-11 23:11:26 -0300

    instalação do axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app').app.use(VueAxios, axios)
